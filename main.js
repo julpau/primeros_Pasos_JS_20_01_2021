805a423a7c4fe49ff0cb6c76f6baa7dd1d14a0a3
Hersain Lozada Hermosa <hersainlozada@gmail.com>
2021-01-20 19:35:17 -0500

    Primeros pasos 20-01-2021

---
+++ main.js
@@ -0,0 +1,34 @@
+// tipos de datos
+
+// numerico
+1
+2
+3
+parseInt(1000)
+
+// caracter
+"Daniel" "Andres" "Pedro"
+
+// array
+['1', '2', '3', '4']
+
+
+// Boolean
+true
+false
+
+
+// Objeto
+
+{
+    nombre: "Miguel"
+    apellido: "Bautista"
+    edad: "23"
+    profesor: "true"
+}
+{
+    nombre: "Diana"
+    apellido: "Bautista"
+    edad: "15"
+    profesor: "true"
+}
